7d1f2c3d62d022841e021c009035c57a24fb4ff7
AntipasG <antipasbelihu@gmail.com>
2024-03-19 14:38:08 +0300

    loop if else  python

---
+++ 0x01-python-if_else_loops_functions/ifelseloop/100-print_tebahpla.py
@@ -0,0 +1,4 @@
+def reverce_alpha (letter):
+    for i in range(ord("z"), ord("a")):
+        print(f"{i}")
+reverce_alpha
---
+++ 0x01-python-if_else_loops_functions/ifelseloop/range.py
@@ -0,0 +1,8 @@
+#printing a tree using the #
+
+#input how long it will be and how wide it should be
+height = int(input("How long is the tree? : "))
+width = int(input("How wide it the tree? : "))
+
+for i in range(width):
+    print(" ")
